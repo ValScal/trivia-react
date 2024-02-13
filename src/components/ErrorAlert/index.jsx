21fa99e6c530606d49feb3b8b4763763194fadaf
Valentina Scalone <148492148+ValScal@users.noreply.github.com>
2024-02-13 16:47:25 +0100

    Update index.jsx

--- src/components/ErrorAlert/index.jsx
+++ src/components/ErrorAlert/index.jsx
@@ -1,4 +1,4 @@
-import styles from "./index.module.css";
+import styles from "/index.module.css";
 
 export default function ErrorAlert({ children }) {
   return <div className={styles.error_alert}>{children}</div>;
