c31fefdbe89923990cae0248d84c3b2496084611
Valentina Scalone <148492148+ValScal@users.noreply.github.com>
2024-02-13 16:47:47 +0100

    Update question.jsx

--- src/components/Question/question.jsx
+++ src/components/Question/question.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
-import styles from "./question.module.css";
+import styles from "/question.module.css";
 import ErrorAlert from "../ErrorAlert";
 
 export default function Question({
